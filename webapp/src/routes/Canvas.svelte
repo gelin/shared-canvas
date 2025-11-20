a4f1df328a5cabb266518fe8eed8d4d6c7abd84b
Denis Nelubin <dnelubin@gmail.com>
2025-11-20 22:03:51 +1300

    Improve drawing.

--- webapp/src/routes/Canvas.svelte
+++ webapp/src/routes/Canvas.svelte
@@ -1,41 +1,56 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
-    onMount(async () => {
-        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-        const context = canvas.getContext('2d');
+    type Event = {
+        offsetX: number;
+        offsetY: number;
+    }
+
+    let canvas: HTMLCanvasElement;
+    let context: CanvasRenderingContext2D | null;
+    let isDrawing = false;
+    let start = { x: 0, y: 0 };
+
+    onMount(() => {
+        context = canvas.getContext('2d');
         if (!context) return;
         context.fillStyle = 'white';
         context.fillRect(0, 0, canvas.width, canvas.height);
-    });
+        context.strokeStyle = 'black';
+        context.lineWidth = 3;  // TODO
+        context.lineCap = 'round';
+    })
 
-    function drawCanvas(event: MouseEvent) {
-        let color = 0;
-        switch (event.buttons) {
-            case 1:
-                color = 0;
-                break;
-            case 2:
-                color = 255;
-                break;
-            case 0:
-            default:
-                return;
-        }
-        // console.log('drawCanvas', event);
-        const canvas = event.target as HTMLCanvasElement;
-        const context = canvas.getContext('2d');
-        const x = event.offsetX;
-        const y = event.offsetY;
-        const imageData = context?.getImageData(x, y, 1, 1);
-        const data = imageData?.data;
-        if (!data) return;
-        data[0] = color; // red
-        data[1] = color; // green
-        data[2] = color; // blue
-        context?.putImageData(imageData, x, y);
+    const handleStart = (({ offsetX: x, offsetY: y }: Event) => {
+        isDrawing = true;
+        start = { x, y };
+    })
+
+    const handleEnd = () => {
+        isDrawing = false
     }
+    const handleMove = (({ offsetX: x1, offsetY: y1 }: Event) => {
+        if (!isDrawing) return;
+        const { x, y } = start;
+        if (!context) return;
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(x1, y1);
+        context.closePath();
+        context.stroke();
+
+        start = { x: x1, y: y1 };
+    })
 </script>
 
-<canvas id="canvas" width="384" height="384" onmousemove={drawCanvas}></canvas>
+<canvas
+        id="canvas"
+        bind:this={canvas}
+        width="384"
+        height="384"
+        onmousedown={handleStart}
+        onmouseup={handleEnd}
+        onmouseleave={handleEnd}
+        onmousemove={handleMove}
+></canvas>
 
