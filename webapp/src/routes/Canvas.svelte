a4c52c75de50e1cad730babf28be57c6433b50ab
Denis Nelubin <dnelubin@gmail.com>
2025-11-21 20:19:50 +1300

    Improve drawing.

--- webapp/src/routes/Canvas.svelte
+++ webapp/src/routes/Canvas.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
-    type Event = {
-        offsetX: number;
-        offsetY: number;
-    }
+    export let width = 384
+    export let height = 384
 
     let canvas: HTMLCanvasElement;
     let context: CanvasRenderingContext2D | null;
     let isDrawing = false;
-    let start = { x: 0, y: 0 };
+    let prev = { x: 0, y: 0 };
+    let lineWidth = 3;
 
     onMount(() => {
         context = canvas.getContext('2d');
@@ -17,40 +16,41 @@
         context.fillStyle = 'white';
         context.fillRect(0, 0, canvas.width, canvas.height);
         context.strokeStyle = 'black';
-        context.lineWidth = 3;  // TODO
+        context.lineWidth = lineWidth;
         context.lineCap = 'round';
     })
 
-    const handleStart = (({ offsetX: x, offsetY: y }: Event) => {
-        isDrawing = true;
-        start = { x, y };
-    })
+    const handleMove = (({ offsetX: x1, offsetY: y1, buttons }: MouseEvent) => {
+        if (!context) return;
+        if (buttons == 1) {
+            if (isDrawing) {
+                const { x, y } = prev;
+                context.beginPath();
+                context.moveTo(x, y);
+                context.lineTo(x1, y1);
+                context.closePath();
+                context.stroke();
+                prev = { x: x1, y: y1 };
+            } else {
+                isDrawing = true;
+                prev = { x: x1, y: y1 };
+            }
+        } else {
+            isDrawing = false;
+        }
+    });
 
     const handleEnd = () => {
-        isDrawing = false
+        isDrawing = false;
     }
-    const handleMove = (({ offsetX: x1, offsetY: y1 }: Event) => {
-        if (!isDrawing) return;
-        const { x, y } = start;
-        if (!context) return;
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(x1, y1);
-        context.closePath();
-        context.stroke();
-
-        start = { x: x1, y: y1 };
-    })
 </script>
 
 <canvas
         id="canvas"
         bind:this={canvas}
-        width="384"
-        height="384"
-        onmousedown={handleStart}
-        onmouseup={handleEnd}
-        onmouseleave={handleEnd}
+        {width}
+        {height}
         onmousemove={handleMove}
+        onmouseleave={handleEnd}
 ></canvas>
 
